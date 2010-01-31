1834800642869e5cb680340cc1571effb51043d1
fdupoux <fdupoux@0ed6bd54-fdb3-4705-a7a1-c7cff8c2153e>
2010-01-31 08:00:10 +0000

    fixed typo
    git-svn-id: https://fsarchiver.svn.sourceforge.net/svnroot/fsarchiver/fsarchiver-trunk@141 0ed6bd54-fdb3-4705-a7a1-c7cff8c2153e

--- src/archreader.h
+++ src/archreader.h
@@ -38,7 +38,7 @@ struct s_archreader
     u32    complevel; // compression level which is specific to the compression algorithm
     u32    fsacomp; // fsa compression level given on the command line by the user
     u64    creattime; // archive create time (number of seconds since epoch)
-    u64    minfsaver; // minimin fsarchiver version required to restore that archive
+    u64    minfsaver; // minimum fsarchiver version required to restore that archive
     u32    hasdirsinfohead; // true if the archive has a "DiRs" header (introduced in 0.6.7)
     int    filefmtver; // set to 1 for "FsArCh_001" or 2 for "FsArCh_002"
     char   filefmt[FSA_MAX_FILEFMTLEN]; // file format of that archive
