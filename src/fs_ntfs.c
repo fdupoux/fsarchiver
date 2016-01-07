1f8926e9d4c3f7db8423113ad41f1aca49f86202
fdupoux <fdupoux@users.sourceforge.net>
2016-01-07 19:12:13 +0000

    Copyright year: 2008-2016

--- src/fs_ntfs.c
+++ src/fs_ntfs.c
@@ -1,7 +1,7 @@
 /*
  * fsarchiver: Filesystem Archiver
  *
- * Copyright (C) 2008-2012 Francois Dupoux.  All rights reserved.
+ * Copyright (C) 2008-2016 Francois Dupoux.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
