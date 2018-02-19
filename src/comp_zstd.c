29cd07bf8a935498d6016bd03f7a83ea6aa802b5
fdupoux <fdupoux@users.sourceforge.net>
2018-02-19 10:33:12 +0000

    Updated Copyright at the top of all source files

--- src/comp_zstd.c
+++ src/comp_zstd.c
@@ -2,7 +2,7 @@
 /*
  * fsarchiver: Filesystem Archiver
  *
- * Copyright (C) 2008-2016 Francois Dupoux.  All rights reserved.
+ * Copyright (C) 2008-2018 Francois Dupoux.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
